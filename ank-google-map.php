5a72b20e16702cfa937b2d3427d5fe5faa168d43
Ankur Kumar <pro.ankurk1@gmail.com>
2014-09-30 11:17:12 +0530

    Update ank-google-map.php

--- ank-google-map.php
+++ ank-google-map.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Ank Google Map
-Plugin URI: http://github.com/ank91
-Description: Simple and non-bloated WordPress Google Map Plugin. Written in pure javascript, no jQuery at all, responsive, configurable, no ads and 100% Free of cost.
-Version: 1.0 (b)
+Plugin URI: http://ank91.github.io/ank-google-map/
+Description: Simple ,light weight, and non-bloated WordPress Google Map Plugin. Written in pure javascript, no jQuery at all, responsive, configurable, no ads and 100% Free of cost.
+Version: 1.1 (b)
 Author: Ankur Kumar
 Author URI: http://www.ankurkumar.hostreo.com
 License: GPL2
 */
-/*  Copyright 214  Ankur Kumar  (email : pro.ankurk1@gmail.com)
+/*  Copyright 2014  Ankur Kumar  (http://github.com/ank91)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -34,11 +34,14 @@ class Ank_Google_Map {
         add_filter( 'plugin_action_links', array( $this, 'plugin_actions_links'), 10, 2 ); //add settings link to plugin list page
         add_action( 'admin_menu', array( $this, 'settings_menu' ) ); // grant admin permission
         add_shortcode('ank_google_map', array( $this, 'agm_shortCode' ));//create a short code
-        add_filter( 'plugin_row_meta', array( $this, 'set_plugin_meta' ), 10, 2 );
+        add_filter( 'plugin_row_meta', array( $this, 'set_plugin_meta' ), 10, 2 );//additional link
+
+
+
         if( false == get_option( 'ank_google_map' ) ) {
-            //save settings first time
-            $this->settings_init();
+            $this->settings_init(); //save settings first time
         }
+
         add_action('admin_enqueue_scripts',array( $this, 'agm_add_color_picker' ));//color picker for settings page
 
 
@@ -74,6 +77,7 @@ class Ank_Google_Map {
         }
         return $links;
     }
+
     function settings_page() {
         //get settings page from this file
         require('agm_options_page.php');
@@ -108,34 +112,34 @@ class Ank_Google_Map {
         add_option( 'ank_google_map', $new_options );
     }
     function agm_add_color_picker() {
-        if( is_admin() ) {
             //available for wp v3.5+
-            // Add the color picker js  +css file
+            // Add the color picker js  + css file (for settings page only)
             wp_enqueue_style( 'wp-color-picker' );
             wp_enqueue_script( 'wp-color-picker' );
-
-        }
     }
 
     /* ********* *********  */
     function write_html_code(){
-        $options = get_option('ank_google_map');
-        $w_unit=($options["div_width_unit"]==='1')?'px':'%';
-        $h_unit=($options["div_height_unit"]==='1')?'px':'%';
-        echo '<div id="agm_map_canvas" style="width:'.$options["div_width"].$w_unit.';height:'.$options["div_height"].$h_unit.';margin: 0 auto;border:1px solid '.$options["div_border_color"].'"></div>';
+        $options =get_option( 'ank_google_map' );
+        $w_unit=($options["div_width_unit"]===1)?'px':'%';
+        $h_unit=($options["div_height_unit"]===1)?'px':'%';
+        echo '<div id="agm_map_canvas" style="width:'.esc_attr($options["div_width"]).$w_unit.';height:'.esc_attr($options["div_height"]).$h_unit.';margin: 0 auto;border:1px solid '.esc_attr($options["div_border_color"]).'"></div>';
     }
 
     function write_js_code(){
         ob_start();
-        $options = get_option('ank_google_map');
+        $options = get_option( 'ank_google_map' );
         $mapType='ROADMAP';
-        if($options['map_type']==='1'){$mapType='ROADMAP';}elseif($options['map_type']==='2'){$mapType='SATELLITE';}elseif($options['map_type']==='3'){$mapType='HYBRID';}elseif($options['map_type']==='4'){$mapType='TERRAIN';}
-        $marker_anim='BOUNCE';
+        if($options['map_type']===1){$mapType='ROADMAP';}
+        elseif($options['map_type']===2){$mapType='SATELLITE';}
+        elseif($options['map_type']===3){$mapType='HYBRID';}
+        elseif($options['map_type']===4){$mapType='TERRAIN';}
+        $marker_anim='DROP';
         if($options['marker_on']==='1'){
-           if($options['maker_anim']=='2'){$marker_anim='BOUNCE';}elseif($options['maker_anim']=='3'){$marker_anim='DROP';}
+           if($options['marker_anim']==2){$marker_anim='BOUNCE';}elseif($options['marker_anim']==3){$marker_anim='DROP';}
         }
         ?>
-        <script src="//maps.googleapis.com/maps/api/js?sensor=false&amp;language=<?php echo $options['map_lang_code']?>"></script>
+        <script src="//maps.googleapis.com/maps/api/js?sensor=false&amp;language=<?php echo esc_attr($options['map_lang_code'])?>"></script>
         <script>
             function loadMap(){
                 var cn = new google.maps.LatLng(<?php echo $options['map_Lat'].','.$options['map_Lng'] ?>);
@@ -144,12 +148,12 @@ class Ank_Google_Map {
                     <?php if($options['map_control_2']==='1'){echo " zoomControl: false, ";} ?>
                     <?php if($options['map_control_3']==='1'){echo " mapTypeControl: false, ";} ?>
                     <?php if($options['map_control_4']==='1'){echo " streetViewControl: false, ";} ?>
-               center: cn, zoom: <?php echo $options['map_zoom'] ?>, mapTypeId: google.maps.MapTypeId.<?php echo $mapType;?>};
+                center: cn, zoom: <?php echo intval($options['map_zoom']) ?>, mapTypeId: google.maps.MapTypeId.<?php echo $mapType;?>};
                 var map = new google.maps.Map(agm, op);
                 <?php if($options['marker_on']==='1') {?>
-                var mk = new google.maps.Marker({position: cn, map:map <?php if($options['marker_anim']!=='1') { echo ", animation: google.maps.Animation.$marker_anim"; }?>, title:"<?php echo $options['marker_title'] ?>" });
+                var mk = new google.maps.Marker({position: cn, map:map <?php if($options['marker_anim']!==1) { echo ", animation: google.maps.Animation.$marker_anim"; }?>, title:"<?php echo esc_attr($options['marker_title']) ?>" });
                 <?php  if($options['info_on']==='1') {?>
-                var iw = new google.maps.InfoWindow({content: "<?php echo $options['info_text']?>"});
+                var iw = new google.maps.InfoWindow({content: "<?php echo addslashes($options['info_text'])?>"});
                 google.maps.event.addListener(map, 'click', function(){iw.close();});
                 <?php } ?>
                 <?php } ?>
@@ -174,9 +178,7 @@ class Ank_Google_Map {
             if (agm){if (typeof google == "object"){google.maps.event.addDomListener(window, "load", loadMap)}
             else {agm.innerHTML = '<h4 style="text-align: center">Failed to load Google Map. Please try again.</h4>'}}</script>
     <?php
-        $output = ob_get_contents();
-        ob_end_clean();
-        echo $this->trim_js($output);
+        echo $this->trim_js(ob_get_clean());
     }
 
     function trim_js($buffer) {
