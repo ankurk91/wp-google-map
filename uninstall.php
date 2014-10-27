c697d04b9834eb8e9b80467b6bc289c2442768ac
ank91 <ank91@users.noreply.github.com>
2014-10-27 13:52:57 +0530

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,17 @@
 <?php
 
-/* this file will be used to remove all traces of this plugin when uninstalled */
+/*  Uninstall file for "Ank Google Map" Plugin
+This file will be used to remove all traces of this plugin when uninstalled
+*/
 
 //if uninstall not called from WordPress do exit
+
 if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
     exit;
 
-
+/*
+ * remove the database entry created by this plugin
+ */
 if(get_option( 'ank_google_map' ) ) {
     delete_option('ank_google_map');
 }
