376cf03ede457ad6f6ea6d28cb4f8175e4aa0cf3
ank91 <pro.ankurk1@gmail.com>
2015-10-19 16:32:46 +0530

    Fix option page js url

--- ank-google-map.php
+++ ank-google-map.php
@@ -3,7 +3,7 @@
 Plugin Name: Ank Google Map
 Plugin URI: https://github.com/ank91/ank-google-map
 Description: Simple, light weight, and non-bloated WordPress Google Map Plugin. Written in pure javascript, no jQuery at all, responsive, configurable, no ads and 100% Free of cost.
-Version: 1.6.2
+Version: 1.6.3
 Author: Ankur Kumar
 Author URI: http://ank91.github.io/
 License: GPL2
@@ -14,7 +14,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 /* no direct access*/
 if (!defined('ABSPATH')) exit;
 
-define('AGM_PLUGIN_VERSION', '1.6.2');
+define('AGM_PLUGIN_VERSION', '1.6.3');
 define('AGM_PLUGIN_SLUG', 'agm_plugin_settings');
 define('AGM_AJAX_ACTION', 'agm_meta_settings');
 
