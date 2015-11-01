d4f08ba33ab089f8cd901886c1444dcd7b538ebd
Ankur Kumar <pro.ankurk1@gmail.com>
2015-11-01 11:10:07 +0530

    Improved option javascript file

--- js/agm-admin-js.js
+++ js/agm-admin-js.js
@@ -1,4 +1,5 @@
-(function (google) {
+(function (document, jQuery) {
+    'use strict';
 
     function $By_ID(a) {
         return document.querySelector('#' + a) || document.getElementById(a);
@@ -8,7 +9,7 @@
         var wd = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         var center = new google.maps.LatLng(agm_opt.map_Lat, agm_opt.map_Lng);
         var opt = {
-            draggable: wd > 480 ? true : false,
+            draggable: wd > 480,
             overviewMapControl: true,
             center: center,
             streetViewControl: false,
@@ -30,7 +31,12 @@
             agm_lng = jQuery('#agm_lng'),
             agm_zoom = jQuery('#agm_zoom'),
             agm_zoom_pre = jQuery('#agm_zoom_pre');
-        var marker = new google.maps.Marker({draggable: true, position: center, map: map, title: 'Current Position'});
+        var marker = new google.maps.Marker({
+            draggable: true,
+            position: center,
+            map: map,
+            title: 'Current Position'
+        });
 
         google.maps.event.addListener(map, 'rightclick', function (event) {
             agm_lat.val(event.latLng.lat());
@@ -81,52 +87,52 @@
         agm_map.innerHTML = '<h4 style="text-align: center;color: #ba060b">Failed to load Google Map.<br>Refresh this page and try again.<br>Check your internet connection as well.</h4>'
     }
 
-})(google);
 
-jQuery(function ($) {
-    /* Prevent form submission when user press enter key in auto-complete */
-    $("#agm_autocomplete").keydown(function (e) {
-        if (e.which == 13 || e.which == 13) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-    });
-    $("#agm_info_on").click(function () {
-        if ($(this).is(":checked"))
-            $(this).next('label').find('i:not(:visible)').fadeIn();
-    });
-    /*check if color picker is available*/
-    if (agm_opt.color_picker) {
-        $('#agm_color_field').wpColorPicker();
-    }
-    /*save screen options*/
-    $('#screen-options-wrap').find('div.agm_meta_box').find('input').change(function () {
-        var params = $(this).parents('div.agm_meta_box').find('input').serialize();
-        var results = $("#agm_meta_ajax_result");
-        results.stop(true, true).fadeOut(0);
-        $.ajax({
-            url: agm_opt.ajax_url,
-            data: params,
-            success: function (d, s) {
-                if (d == '1' && s == 'success') {
-                    results.html('&#10004; Settings has been saved, <a href="#" onclick="window.location.reload()">Reload</a> page to see changes.').fadeIn(function () {
-                        results.delay('10000').fadeOut(0);
-                    });
-                }
-            }, error: function (a, b) {
-                var err = 'Error Saving Options';
-                if (a.status === 0) {
-                    err = "Could not connect to server ! Try Again..";
-                } else {
-                    if (b === "timeout") {
-                        err = "Connection Timeout ! Try Again..";
-                    }
-                    else {
-                        err = "Unknown error.";
-                    }
-                }
-                results.html('&#10008; ' + err).show();
+    jQuery(function ($) {
+        /* Prevent form submission when user press enter key in auto-complete */
+        $("#agm_autocomplete").keydown(function (e) {
+            if (e.which == 13 || e.which == 13) {
+                e.preventDefault();
+                e.stopPropagation();
             }
         });
+        $("#agm_info_on").click(function () {
+            if ($(this).is(":checked"))
+                $(this).next('label').find('i:not(:visible)').fadeIn();
+        });
+        /*check if color picker is available*/
+        if (agm_opt.color_picker) {
+            $('#agm_color_field').wpColorPicker();
+        }
+        /*save screen options*/
+        $('#screen-options-wrap').find('div.agm_meta_box').find('input').change(function () {
+            var params = $(this).parents('div.agm_meta_box').find('input').serialize();
+            var results = $("#agm_meta_ajax_result");
+            results.stop(true, true).fadeOut(0);
+            $.ajax({
+                url: agm_opt.ajax_url,
+                data: params,
+                success: function (d, s) {
+                    if (d == '1' && s == 'success') {
+                        results.html('&#10004; Settings has been saved, <a href="#" onclick="window.location.reload()">Reload</a> page to see changes.').fadeIn(function () {
+                            results.delay('10000').fadeOut(0);
+                        });
+                    }
+                }, error: function (a, b) {
+                    var err = 'Error Saving Options';
+                    if (a.status === 0) {
+                        err = "Could not connect to server ! Try Again..";
+                    } else {
+                        if (b === "timeout") {
+                            err = "Connection Timeout ! Try Again..";
+                        }
+                        else {
+                            err = "Unknown error.";
+                        }
+                    }
+                    results.html('&#10008; ' + err).show();
+                }
+            });
+        });
     });
-});
+})(document, jQuery);
